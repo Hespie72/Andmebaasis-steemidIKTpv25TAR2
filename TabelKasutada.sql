a3816e428f459922fb595292b875d7cd8920d113
Vitalii Abdulov <vitalya.abdulov.1979@gmail.com>
2026-02-02 16:18:28 +0200

    Enhance TabelKasutada.sql with data manipulation queries
    Added SELECT, DELETE, and UPDATE statements to manage 'opilane' records.

--- TabelKasutada.sql
+++ TabelKasutada.sql
@@ -3,6 +3,14 @@ INSERT INTO opilane(eesnimi, perenimi, synniaeg, kas_opib)
 VALUES ('Vitalii', 'Sid', '2010-04-29', 0),
 ('Vitalii', 'Lavrov', '2010-04-29', 0),
 ('Vitalii', 'Nikitin', '2010-04-29', 0);
+SELECT * from opilane;
+--ühe kirja kustutamine
+DELETE FROM opilane WHERE opilaneID=6; 
 Select * from opilane;
 --muudame tabeli ja lisame piirangu
 ALTER TABLE opilane Alter column perenimi varchar(30) UNIQUE;
+kirje uuendamine
+SELECT * FROM opilane
+UPDATE opilane SET aadress='Tallinn'
+--WHERE opilaneID=4
+SELECT * FROM opilane;
